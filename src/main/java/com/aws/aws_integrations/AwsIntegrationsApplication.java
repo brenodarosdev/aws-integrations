34a9f5d7bec1385f859421d36e897d8592213696
Breno Daros <brenosouzadaros@gmail.com>
2024-12-04 19:56:59 -0300

    initial commit

---
+++ src/main/java/com/aws/aws_integrations/AwsIntegrationsApplication.java
@@ -0,0 +1,13 @@
+package com.aws.aws_integrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsIntegrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsIntegrationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/aws_integrations/AwsIntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.aws_integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsIntegrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
