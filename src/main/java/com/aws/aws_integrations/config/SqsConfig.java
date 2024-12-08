70678ca00ffe52d229129f034b8092dfb89556ce
Breno Daros <brenosouzadaros@gmail.com>
2024-12-08 18:38:37 -0300

    chore: adjust values passed in SqsConfig

--- src/main/java/com/aws/aws_integrations/config/SqsConfig.java
+++ src/main/java/com/aws/aws_integrations/config/SqsConfig.java
@@ -1,5 +1,6 @@
 package com.aws.aws_integrations.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -10,15 +11,17 @@ import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import java.net.URI;
 
 @Configuration
+@RequiredArgsConstructor
 public class SqsConfig {
+    private final AwsConfigProperties awsSqsProperties;
 
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
         return SqsAsyncClient.builder()
-                .endpointOverride(URI.create("http://localhost:4566"))
-                .region(Region.US_EAST_1)
+                .endpointOverride(URI.create(awsSqsProperties.getEndpointuri()))
+                .region(Region.of(awsSqsProperties.getRegion()))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("test", "test")))
+                        AwsBasicCredentials.create(awsSqsProperties.getAccesskey(), awsSqsProperties.getSecretkey())))
                 .build();
     }
 }
